50eb5130410542246d50a6ec2e98819d1568ed17
Niek Candaele <niekcandaele@gmail.com>
2017-10-18 16:22:01 +0200

    Initial commit

---
+++ src/assets/vue/pages/sevendtdserver.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+    <f7-page>
+        <f7-navbar title="El Server" back-link="Back" sliding></f7-navbar>
+        <f7-block inner>
+            <p>This is some info about Elanio's server</p>
+        </f7-block>
+
+        <f7-block inner>
+            <p>{{getStatsData}}</p>
+            <p>
+                <strong>Errors</strong> <br> {{errors}}</p>
+        </f7-block>
+    </f7-page>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      getStatsData: "test",
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://localhost:3000/api/getstats?guild=317995576958648321`)
+    .then(response => {
+      this.getStatsData = response
+    })
+    .catch(e => {
+        console.log(e)
+      this.errors.push(e)
+    })
+  }
+}
+</script>
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+export default [{
+        path: '/about/',
+        component: require('./assets/vue/pages/about.vue')
+    },
+    {
+        path: '/form/',
+        component: require('./assets/vue/pages/form.vue')
+    },
+    {
+        path: '/dynamic-route/blog/:blogId/post/:postId/',
+        component: require('./assets/vue/pages/dynamic-route.vue')
+    },
+    {
+        path: '/7dtdserver',
+        component: require('./assets/vue/pages/sevendtdserver.vue')
+    }
+]
