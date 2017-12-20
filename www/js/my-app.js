38ff23812c5c89150857491dbcd590ceddef9fe8
Niek Candaele <niekcandaele@gmail.com>
2017-12-20 13:08:03 +0100

    Added F7 cordova template

---
+++ www/js/my-app.js
@@ -0,0 +1,67 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+
+$$('#addServer-form').submit(function(event) {
+    addServer()
+    console.log('submitted form')
+    return false
+})
+
+function addServer(data) {
+    var formData = myApp.formToData('#addServer-form')
+    let user
+
+
+    $$.post('http://localhost:1337/user', {
+        steamId: Math.floor((Math.random() * 1000000) + 1),
+        username: formData.username
+    }, function success(response, status, xhr) {
+        console.log('Created a new user')
+        user = response
+        $$.post('http://localhost:1337/api/sdtdserver/addserver', {
+            serverip: formData.serverip,
+            telnetport: formData.telnetport,
+            telnetpassword: formData.telnetpassword,
+            webport: formData.webport,
+            ownerid: user.id
+        }, function success(response, status, xhr) {
+            console.log('Added server')
+            console.log(response)
+        }, function error(response, status) {
+            console.log('error adding server')
+            console.log(response)
+        })
+    }, function error(response, status) {
+        console.log('Error creating a user')
+        console.log(response)
+        alert(status)
+    })
+
+
+   
+}
